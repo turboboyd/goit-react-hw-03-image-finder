30a23ed213a9cdf13793e0256ca52a63efc17939
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-07-28 16:37:24 +0200

    Fix

--- src/servises/api-images.js
+++ src/servises/api-images.js
@@ -1,17 +1,18 @@
-import axios from 'axios';
-
-export const fetchImages = async (name, page) => {
+export const fetchImages = (name, page) => {
   const KEY = '37071230-d6b04d3068f1a0950a5b376a5';
   const BASE_URL = 'https://pixabay.com/api/';
 
-  try {
-    const response = await axios.get(
-      `${BASE_URL}?q=${name}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
-    );
-    return response.data;
-  } catch (error) {
-    return Promise.reject(
-      new Error('An error has occurred, please try again!')
-    );
-  }
+  return fetch(
+    `${BASE_URL}?q=${name}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error('Error fetching images:', error);
+      throw new Error('An error has occurred, please try again!');
+    });
 };
